ca0d49460bd1125d1ceccaae83551060c9e0befc
meenakirann <112334877+meenakirann@users.noreply.github.com>
2022-09-21 19:13:47 +0530

    Update Residence1.java

--- src/org/testing/Residence1.java
+++ src/org/testing/Residence1.java
@@ -3,11 +3,8 @@ package org.testing;
 public class Residence1 implements Residence  {
 
 	public void nellore1() {
-<<<<<<< HEAD
+
 		System.out.println("nellore7");
-=======
-		System.out.println("nellore3");
->>>>>>> 644cd58ffc6e546920067b61731345597505b904
 		
 	}
 	public void nellore() {
